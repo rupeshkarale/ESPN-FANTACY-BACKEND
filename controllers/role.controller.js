951396c27323205ab74ed45156e2194fdd3ed584
Rupesh Karale <rupeshkarale3245@gmail.com>
2024-06-07 12:52:51 +0530

    matches and team added

--- controllers/role.controller.js
+++ controllers/role.controller.js
@@ -1,36 +1,22 @@
 const express = require("express");
-const Role = require("../models/role.model");
+const matches = require('../data/match.json')
+const teams = require('../data/team.json')
 const roleRouter = express.Router();
 
-roleRouter.post("/role", async (req, res, next) => {
-  const { name } = req.body;
-  const exist = await Role.exists({ name: name });
-
-  if (name != "user" && name != "admin") {
-    return res.send({
-      success: false,
-      message: `Role ${name} is invalid`,
-    });
-  }
-
-  if (exist) {
-    return res.send({
-      success: false,
-      message: `Role with ${name} is already present`,
-    });
-  } else {
-    next();
-  }
-});
-
-// insert role
-roleRouter.post("/role", async (req, res) => {
-  const role = Role(req.body);
-  await role.save();
+roleRouter.get("/matches", async (req, res) => {
   res
     .send({
       sucess: true,
-      id: role._id,
+      data: matches,
+    })
+    .status(201);
+});
+roleRouter.get("/teams", async (req, res) => {
+
+  res
+    .send({
+      sucess: true,
+      data: teams,
     })
     .status(201);
 });
